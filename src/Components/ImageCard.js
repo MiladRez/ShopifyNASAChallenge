9c6517ddc1b7f76ecc10c3bb8360127fb52687c8
MiladRez <miladrezaee07@gmail.com>
2022-01-19 21:28:33 -0500

    Final commit, centered like icon

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -25,14 +25,21 @@ const ImageCard = ({title, date, img, darkMode, numOfImg}) => {
     var fontColor = darkMode ? "fontColorDark" : "fontColorLight";
     var bgColor = darkMode ? "bgColorCardDark" : "bgColorCardLight";
 
+    var watched = Math.floor(Math.random() * 999) + 100;
+
     return (
         <div className="column" style={{gridRowEnd: `span ${span}`}}>
             <div className={`ui segment ${bgColor}`} ref={cardRef} style={{width: 480}}>
                 <img ref={imageRef} src={img} alt="Sorry, image not available." />
                 <div style={{paddingTop: "10px", paddingLeft: "5px"}}>
                     <h3 className={fontColor}>{title}</h3>
+                    <span className='heartIcon'><Heart isClick={liked} onClick={() => {setLiked(!liked)}} /></span>
                     <p className={fontColor}>{date}</p>
-                    <Heart isClick={liked} onClick={() => {setLiked(!liked)}} />
+                    <div className='eyecon'>
+                        <i className={`eye icon ${fontColor}`} style={{fontSize: "25px", display: "inline-flex"}}>
+                            <span style={{fontFamily: "Lato", marginLeft: "10px"}}> {watched}</span>
+                        </i>
+                    </div>
                 </div>
             </div>
         </div>
